3579306f5343f2169de07fa3f365958584ea3b79
Ivan <maverickmsk1@gmail.com>
2023-03-21 20:12:08 +0300

    UI layout updated

--- main.py
+++ main.py
@@ -8,46 +8,57 @@ from src.logic import calculate_ts_metrics
 
 settings = get_settings()
 
-st.title("Time series prediction app")
+st.title("Time series prediction UI")
 
-with st.form("main_form"):
-    session = requests.Session()
-
-    feature_name = st.text_input("Feature name", "post_views_per_day")
-
-    smoothing_level = st.slider(
-        "Select smoothing level", min_value=0.0, max_value=1.0, step=0.05, value=0.5
+csv_file = st.file_uploader(
+    "Upload csv file with time series data", type=["csv"]
     )
-    csv_file = st.file_uploader("Upload csv file", type=["csv"])
-    send_req = st.form_submit_button("Predict !")
-    
 
-    if csv_file:
-        st.subheader("Time series info")
+if csv_file:
+    with st.form("main_form"):
+        st.header("Prediction parameters")
 
         df = pd.read_csv(csv_file)
-        if feature_name not in df.columns:
-            st.error("Select valid feature name !")
+        feature_name = st.selectbox("Pick a feature to predict", options=df.columns)
+        smoothing_level = st.slider(
+            "Select smoothing level",
+            min_value=0.0, max_value=1.0, step=0.05, value=0.5
+        )
 
-        values = df[feature_name].values        
+        payload = TimeSeries(
+            feature=feature_name,
+            data=list(df[feature_name].values),
+            smoothing_level=smoothing_level,
+        ).json()
 
-        payload = TimeSeries(feature=feature_name, data=list(values), smoothing_level=smoothing_level).json()
+        if st.form_submit_button("Calculate metrics and make prediction"):
+            session = requests.Session()
 
-        st.line_chart(data=df, y=feature_name)
+            st.subheader("Selected time series")
+            st.line_chart(data=df, y=feature_name)
 
-        metrics = calculate_ts_metrics(values)
+            st.subheader("Calculated metrics")
+            metrics = calculate_ts_metrics(df[feature_name].values)
 
-        m1, m2 = st.columns(2)
-        m1.metric(label="Mean", value=metrics["mean"])
-        m2.metric(label="Std", value=metrics["std"])
+            m1, m2 = st.columns(2)
+            m1.metric(label="Mean", value=metrics["mean"])
+            m2.metric(label="Std", value=metrics["std"])
 
+            st.markdown("---")
+            st.subheader("Prediction result")
 
-    if send_req:
-        try:
-            st.header("Prediction result")
-            response = session.post(settings.ml_service_url, payload).json()
-            c1, c2 = st.columns(2)
-            c1.text("Predicted value: ")
-            c2.text(round(response["predicted_value"], 3))
-        except requests.exceptions.ConnectionError as e:
-            st.warning("Can't connect to service !")
+            try:
+                response = session.post(settings.ml_service_url, payload).json()
+
+                pred = round(response["predicted_value"], 3)
+
+                c11, c12 = st.columns(2)
+                c11.text("Feature name: ")
+                c12.markdown(f"`{response['feature']}`")
+
+                c21, c22 = st.columns(2)
+                c21.text("Predicted value: ")
+                c22.markdown(f"`{pred}`")
+
+            except requests.exceptions.ConnectionError as e:
+                st.warning("Can't connect to service !")
