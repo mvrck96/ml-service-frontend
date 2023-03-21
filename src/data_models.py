5a2be00568f86e04fe203f149c2b885b770fefe3
Ivan <maverickmsk1@gmail.com>
2023-03-21 20:18:44 +0300

    removed unused model

--- src/data_models.py
+++ src/data_models.py
@@ -2,12 +2,10 @@ from typing import List
 
 from pydantic import BaseModel
 
+
 class TimeSeries(BaseModel):
+    """Request data model"""
+
     feature: str
     data: List[float]
     smoothing_level: float
-
-class PredictedValue(BaseModel):
-    feature: str
-    predicted_value: float
-    
