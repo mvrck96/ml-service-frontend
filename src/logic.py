77700845d48590f272daea75044a82f823dfb933
Ivan <maverickmsk1@gmail.com>
2023-03-21 20:18:53 +0300

    docstring added

--- src/logic.py
+++ src/logic.py
@@ -2,9 +2,15 @@ from typing import List, Dict
 
 from numpy import mean, std, round
 
+
 def calculate_ts_metrics(ts: List[float]) -> Dict[str, float]:
+    """Calculate time series metrics.
+
+    @param ts[List[float]]: Time series to calculate metrics from
+    @return [Dict[str, float]]: Dict of metrics
+    """
     metrics = dict().fromkeys(["mean", "std"])
-    
+
     metrics["mean"] = round(mean(ts), 3)
     metrics["std"] = round(std(ts), 3)
     return metrics
